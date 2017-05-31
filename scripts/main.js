de5ca7dee264ef23ff1bda35c87b45c1ede21daa
mdsirio <rcbsirio@gmail.com>
2017-05-31 10:43:23 -0400

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,28 @@
+var myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    var mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/firefox.jpg') {
+        myImage.setAttribute('src', 'images/squirrel.jpeg');
+    } else {
+        myImage.setAttribute('src', 'images/firefox.jpg');
+    }
+}
+
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+myButton.onclick = function() {
+    setUserName();
+}
+
+if (!localStorage.getItem('name')) {
+    setUserName();
+} else {
+    var storedName = localStorage.getItem('name');
+    myHeading.textContent = 'Mozilla is cool, ' + storedName;
+}
+function setUserName() {
+    var myName = prompt('Please enter your name.');
+    localStorage.setItem('name', myName);
+    myHeading.textContent = 'Mozilla is cool, ' + myName;
+}
